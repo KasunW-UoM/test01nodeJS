6aebf540e5579aeb60d586d67602c7f5fbdce2e1
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-05 22:11:07 +0530

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,37 @@
+ /*
+ 
+ const a =20;
+ const b="20";
+
+ if(a==b){
+     console.log("value ture");
+ }
+
+ */
+
+ const input ="A";
+
+ if(input == "A"){
+     console.log("Excellent");
+ }
+ else if(input == "B"){
+    console.log("Very Good ");
+}
+else if(input == "C"){
+    console.log("Good");
+}
+else if(input == "S"){
+    console.log("high pass");
+}
+else if(input == "D"){
+    console.log("Low Pass");
+}
+else if(input == "W"){
+    console.log("Fail");
+}
+else
+{
+    console.log("Input is not Valid...");
+}
+
+
---
+++ user-loggin.js
@@ -0,0 +1,21 @@
+//input
+
+const userName ="kasun";
+const userPasword="2021";
+
+//DB
+
+const dbuserName ="kasun";
+const dbuserPasword="2021";  //mehi if eka ture wenne DB eka saha input eka equal nam pamani...
+
+if((userName==dbuserName ) && (userPasword==dbuserPasword)){
+
+
+    console.log("Welcome Have A Nice Day/......");
+
+}
+else{
+
+    console.log("TRy Again....");
+
+}
