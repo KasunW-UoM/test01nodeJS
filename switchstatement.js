9d558961d8fdbb6d32ea58c270c9241666082e9a
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-06 23:36:38 +0530

    Add files via upload

---
+++ switchstatement.js
@@ -0,0 +1,19 @@
+const result ="A";
+
+switch (result){
+
+    case "A":
+        console.log("Excellent");
+        break;
+        case "B":
+            console.log("Very Good");
+            break;
+            case "C":
+                console.log("Good");
+                break;
+                
+                default:
+                    console.log("Not Valid....");
+
+
+}
