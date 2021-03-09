bfbcf0ac72022b535f020eb96ba9460624c12aeb
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-09 22:45:30 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,40 @@
+/*for(let i=0;i<10;i++){
+
+    console.log("kasun",i);
+}
+
+//ihatha loop eka break kirimata awashya nam pahatha paridi eya sidu karai...
+
+for(let i=0;i<10;i++){
+
+    if(i==5){
+
+        break;
+    }
+
+    console.log("kasun",i);
+}
+
+
+=================================================
+*/
+// while loop
+/*
+let i=0;
+while(i<10){
+
+    console.log("Hello World",i);
+    i++;
+}
+*/
+//=====================================================
+
+//do whlie loop
+
+let i=0;
+
+do{
+
+    console.log("Hello World",i);
+    i++;
+}while(i<10);
