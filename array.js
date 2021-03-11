b69f77e3ce3efa80ab30976c365729a59b9b57e6
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-11 08:52:50 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,39 @@
+let name=["kasun","madhulal ","Weerasekara"]; //array yanu object data type ekaki....
+
+// array hadunwanne index anuwai...
+/*
+console.log(name);
+
+console.log(name[0]);
+
+*/
+
+//=======================================
+// array ekak value wenas karana akaraya.......
+
+/*
+name[1]="M.";
+console.log(name);
+
+console.log(name[1]);
+
+*/
+
+///ARRAY EKAK SIZE EKA SOYANA AKARAYA
+/*
+console.log(name.lenth);
+
+*/
+
+for(let names of name){
+    console.log(names);
+}
+
+
+//array walata elements ekathu karana widiha...
+
+name[3]="zenlotus";
+console.log(name);
+
+name.push("cinnomon");
+console.log(name);
