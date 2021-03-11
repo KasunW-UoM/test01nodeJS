2db6d5546e96f4c984aa2bcce4dddc19e76aee85
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-11 08:52:31 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,32 @@
+
+/*
+function Hello(){
+    console.log("Hello");
+}
+
+Hello();
+
+
+
+function squre(num){
+
+    return num*num;
+
+}
+const x=squre(4);
+console.log(x);
+*/
+
+//es6 functions
+/*
+const squre = (x)=>{
+     return x*x ;   
+    
+    
+    };
+
+    console.log(squre(6) );
+    */
+
+    const squre = (x)=>x*x;
+    console.log(squre(8) );
